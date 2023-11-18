770454a8db224f04ed28717bd1752c15c293818c
trdp30 <trdp30@gmail.com>
2023-11-18 20:21:53 +0530

    admin access redirection handled

--- src/components/AdminLayout/index.js
+++ src/components/AdminLayout/index.js
@@ -4,16 +4,22 @@ import AdminNavBar from "../AdminNavBar";
 import AuthContext from "contexts/AuthContext";
 import { includes } from "lodash";
 import roles from "utils/roles";
+import Loader from "components/Loader";
 
 function AdminLayout() {
-  const { currentUserRoles, isAuthenticating } = useContext(AuthContext);
+  const { currentUserRoles } = useContext(AuthContext);
+  const canAccess = includes(currentUserRoles, roles.admin);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isAuthenticating && !includes(currentUserRoles, roles.admin)) {
+    if (!canAccess) {
       navigate("/");
     }
-  }, [isAuthenticating, currentUserRoles, navigate]);
+  }, [canAccess, navigate]);
+
+  if (!canAccess) {
+    return <Loader />;
+  }
 
   return (
     <>
